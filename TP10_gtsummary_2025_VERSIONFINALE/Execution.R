c2b8c92dddae87e1bb14b7cf4648ee07e13c45d4
awa-d <160314560+awa-d@users.noreply.github.com>
2025-04-20 23:12:37 +0000

    Delete TP10_gtsummary_2025_VERSIONFINALE directory

--- TP10_gtsummary_2025_VERSIONFINALE/Execution.R
+++
@@ -1,8 +0,0 @@
-# Installer bookdown si nécessaire
-install.packages("bookdown")
-
-# Générer le livre en HTML
-bookdown::render_book("pdg.Rmd", "bookdown::gitbook")
-
-# Générer le livre en PDF
-#bookdown::render_book("pdg.Rmd", "bookdown::pdf_book")
